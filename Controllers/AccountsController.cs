7106042c652ee2fceedd24810e7ca736a716f782
ucwealth <ucheokeke16@gmail.com>
2020-06-02 18:42:15 +0100

    added register view

--- Controllers/AccountsController.cs
+++ Controllers/AccountsController.cs
@@ -3,8 +3,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BelleMata.Controllers
 {
+    [Controller]
     public class AccountsController : Controller
     {
+        
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
         public IActionResult Register(CreateUserRequestModel model)
         {
             return View();
